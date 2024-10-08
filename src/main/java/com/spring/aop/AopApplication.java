e53dab090b65485947126543daf4731c90c8eca2
PC1237 <simovanev@abv.bg>
2024-10-08 12:35:39 +0300

    Initial commit

---
+++ src/main/java/com/spring/aop/AopApplication.java
@@ -0,0 +1,23 @@
+package com.spring.aop;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello");
+        };
+    }
+}
+
+
