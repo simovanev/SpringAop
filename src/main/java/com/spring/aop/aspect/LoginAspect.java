a0e09a0c7d78aa50fd85f0b385fe3c6019af3f85
PC1237 <simovanev@abv.bg>
2024-10-09 12:46:59 +0300

    @Pointcut declaration

--- src/main/java/com/spring/aop/aspect/LoginAspect.java
+++ src/main/java/com/spring/aop/aspect/LoginAspect.java
@@ -2,12 +2,16 @@ package com.spring.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoginAspect {
-    @Before("execution(public void addAccount())")
+
+    @Pointcut("execution(* com.spring.aop.dao.*.*(..))")
+    private void forDaoPackage(){}
+    @Before("forDaoPackage()")
     public  void beforeAddAccountAdvice(){
         System.out.println("Before add account advice");
     }
