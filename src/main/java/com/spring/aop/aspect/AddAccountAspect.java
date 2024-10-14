07f394e4a4391d88a3ae89664323b68584b2fdbf
Simovanev <simovanev@abv.bg>
2024-10-14 23:47:19 +0300

    @AfterReturning advise and modify the results of the method

--- src/main/java/com/spring/aop/aspect/AddAccountAspect.java
+++ src/main/java/com/spring/aop/aspect/AddAccountAspect.java
@@ -3,6 +3,7 @@ package com.spring.aop.aspect;
 import com.spring.aop.Account;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
@@ -14,24 +15,33 @@ import java.util.List;
 @Component
 @Order(2)
 public class AddAccountAspect {
+    @AfterThrowing(
+            pointcut = "(execution(* com.spring.aop.dao.AccountDAO.getAccount(..)))",
+            throwing = "exc")
+    public void afterThrowing(JoinPoint joinPoint, Throwable exc) {
+        System.out.println("AfterThrowing " + joinPoint.getSignature().getName());
+        System.out.println("The exception is " + exc);
+    }
+
+
     @AfterReturning(
-            pointcut = "(execution(* com.spring.aop.dao.AccountDAO.getAccounts()))",
+            pointcut = "(execution(* com.spring.aop.dao.AccountDAO.getAccount()))",
             returning = "result"
 
     )
-    public void doAfterReturning(JoinPoint joinPoint, List<Account> result){
+    public void doAfterReturning(JoinPoint joinPoint, List<Account> result) {
         System.out.println("+++++++After+++++++");
         System.out.println(joinPoint.getSignature().toShortString());
-        System.out.println("Result "+result);
+        System.out.println("Result " + result);
 
         changeTheNamesToUpperCase(result);
         for (Account account : result) {
-            System.out.print(account.getName()+"  ");
+            System.out.print(account.getName() + "  ");
         }
     }
 
     private void changeTheNamesToUpperCase(List<Account> result) {
-        for(Account a : result) {
+        for (Account a : result) {
             a.setName(a.getName().toUpperCase());
         }
     }
